60bd361f63c945025b30cb19e8e0e3d6e0b78ba5
Eddie Payne <eddie_p89@hotmail.com>
2023-12-11 17:47:36 +1100

    Added comments to script.js file

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function generatePassword() {
   alert("Your unique password must be between 20 and 128 characters");
 
   var passwordLength = prompt("How long you would like you password to be?");
+  // convert input string to an 'Int'
   var userInput = parseInt(passwordLength);
 
   if (userInput <= 20 || userInput >= 128) {
@@ -29,7 +30,7 @@ function generatePassword() {
   }
 
 
-  //// Variables for Password options
+  //// Variables for Password options depending on user's input
 
   var includeLowercase = confirm("Would you like to use any lowercase letters?");
 
@@ -51,54 +52,52 @@ function generatePassword() {
 
 
   // IF Statement to handle user input
-
-  // if (passwordLength == NaN || passwordLength > 20 || passwordLength < 128) {
-  //   alert("Password length must be between 20 and 128");
-  // }
-
-  var allChars = "";
-
-  if (includeLowercase === true) {
-    allChars += lowercaseChars;
-  }
-  if (includeUppercase === true) {
-    allChars += uppercaseCaseChars;
-  }
-  if (includeNumbers === true) {
-    allChars += numberChars;
-  }
-  if (includeSpecChars === true) {
-    allChars += specialChars;
-  }
-  if (allChars === "") {
-    alert("Please select at least one character type.");
-    return;
-
-  }
-
-
-  // generate password
-  console.log(allChars)
-
-  var password = "";
   
-  for(i = 0; i < userInput; i++) {
-    var randomLetter = allChars[Math.floor(Math.random() * allChars.length)]
-    // console.log(randomLetter)
-    password += randomLetter
-  }
+    var allChars = "";
+  
+    if (includeLowercase === true) {
+      allChars += lowercaseChars;
+    }
+    if (includeUppercase === true) {
+      allChars += uppercaseCaseChars;
+    }
+    if (includeNumbers === true) {
+      allChars += numberChars;
+    }
+    if (includeSpecChars === true) {
+      allChars += specialChars;
+    }
+    if (allChars === "") {
+      alert("Please select at least one character type.");
+      return;
+  
+    }
 
-  return password;
+    
+      console.log(allChars)
+    // declaring password as an empty string
+      var password = "";
+    
+      // FOR loop to produce random character based on user's input
+      for (i = 0; i < userInput; i++) {
+        var randomLetter = allChars[Math.floor(Math.random() * allChars.length)]
+        // console.log(randomLetter)
+        password += randomLetter
+      }
+    // Return password to Generate Password function
+      return password;
+    
+    }
+    
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
+    
+    
+    
+    
 
-}
 
-
-
-
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+    
 
 
 
