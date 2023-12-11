34a79f67515a60c3860f5e333dc1a6231aaced31
Eddie Payne <eddie_p89@hotmail.com>
2023-12-11 17:43:59 +1100

    Edited script.js file - fixed bugs in lines 23-28

--- script.js
+++ script.js
@@ -1,62 +1,99 @@
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-
 
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
+
   passwordText.value = password;
+
+}
+
+// Function to generate a password
+
+function generatePassword() {
+
+  alert("Welcome to the Random Password Generator!");
+  alert("Your unique password must be between 20 and 128 characters");
+
+  var passwordLength = prompt("How long you would like you password to be?");
+  var userInput = parseInt(passwordLength);
+
+  if (userInput <= 20 || userInput >= 128) {
+    alert("Password length must be between 20 and 128");
+    return;
+  }
+
+
+  //// Variables for Password options
+
+  var includeLowercase = confirm("Would you like to use any lowercase letters?");
+
+  var includeUppercase = confirm("Would you like to use any UPPERCASE letters?");
+
+  var includeNumbers = confirm("Would you like to use any numbers?");
+
+  var includeSpecChars = confirm("Would you like to use any special characters?");
+
+  var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
+
+  var uppercaseCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  var numberChars = '123456789';
+
+  var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+  // var allChars = "";
+
+
+  // IF Statement to handle user input
+
+  // if (passwordLength == NaN || passwordLength > 20 || passwordLength < 128) {
+  //   alert("Password length must be between 20 and 128");
+  // }
+
+  var allChars = "";
+
+  if (includeLowercase === true) {
+    allChars += lowercaseChars;
+  }
+  if (includeUppercase === true) {
+    allChars += uppercaseCaseChars;
+  }
+  if (includeNumbers === true) {
+    allChars += numberChars;
+  }
+  if (includeSpecChars === true) {
+    allChars += specialChars;
+  }
+  if (allChars === "") {
+    alert("Please select at least one character type.");
+    return;
+
+  }
+
+
+  // generate password
+  console.log(allChars)
+
+  var password = "";
   
+  for(i = 0; i < userInput; i++) {
+    var randomLetter = allChars[Math.floor(Math.random() * allChars.length)]
+    // console.log(randomLetter)
+    password += randomLetter
+  }
+
+  return password;
+
 }
 
 
-// Object containg all aceptable password characters 
-var charOptions = {
-lowercase: 'abcdefghijklmnopqrstuvwxyz0123456789',
-uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-number: '123456789',
-symbol:  "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
-};
 
-// Password criteria prompts
-
-  window.alert("Welcome to the Random Password Generator!");
-
-window.alert("Your unique password must be between 20 and 128 characters");
-
-window.prompt("How long you would like you password to be?");
-
-
-// Variables for Password options
-
-var includeLowercase = confirm("Would you like to use any lowercase letters?");
-
-var includeUppercase = confirm("Would you like to use any UPPERCASE letters?");
-
-var includeNumbers = confirm("Would you like to use any numbers?");
-
-var includeSpecChars = confirm("Would you like to use any special characters?");
-
-var lowercaseChars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-
-var uppercaseCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-var numberChars = '123456789';
-
-var specialChars = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-
-var allChars = "";
-
-
-
-
-// IF Statement to handle user input
-
-if (includeLowercase === true)
 
 
 
@@ -67,7 +104,6 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-writePassword();
 
 
 
